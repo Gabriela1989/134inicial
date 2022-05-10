76e7564d47856283bc5df5df0620972a6a0a72d8
dell <gabrielalima16@gmail.com>
2022-05-10 19:38:09 -0300

    Teste de Unidade usando uma lista como massa de teste

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import somar, subtrair, multiplicar, dividir
 
 
@@ -40,7 +42,7 @@ def teste_multiplicar():
     assert resultado_obtido == resultado_esperado
 
 
-def teste_dividir():
+def teste_dividir_positivo():
     # 1 - Configurar
     numero_a = 20
     numero_b = 2
@@ -64,6 +66,28 @@ def teste_dividir_negativo():
 
     # 2 - Executa
     resultado_obtido = dividir(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+
+# 1 lista para uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+]
+
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+    # 1 - Configura
+    # utilizamos a lista como massa de teste
+
+    # 2 - Executa
+    resultado_obtido = somar(numero_a, numero_b)
 
     # 3 - Valida
     assert resultado_obtido == resultado_esperado
